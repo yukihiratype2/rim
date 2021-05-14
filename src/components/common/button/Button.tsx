1b20ea145eca27f7aef3ff14cec016d55865b959
yukihiratype2 <yukihiratype2@live.com>
2021-05-14 10:04:47 +0800

    init

--- src/components/common/button/Button.tsx
+++
@@ -1,7 +0,0 @@
-export type ButtonProps = {
-  color?: string
-} & React.ComponentProps<'button'>;
-
-const Button = ({ color, ...props }: ButtonProps) => <button type="button" {...props} />;
-
-export default Button;
---
+++ src/views/auth/login/LoginForm.tsx
@@ -0,0 +1,11 @@
+import { Button, TextField } from '@material-ui/core';
+
+const LoginForm = () => (
+  <form>
+    <TextField variant="outlined" placeholder="Username" />
+    <TextField variant="outlined" placeholder="Username" type="password" />
+    <Button />
+  </form>
+);
+
+export default LoginForm;
